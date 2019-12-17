9c11ea293472b594ce534933d75b0f4c29134d43
Subramaniam <subramaniam.vls@gmail.com>
2019-12-17 20:44:40 +0530

    OK

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,21 +1,12 @@
-import fs from 'fs';
+import { CsvFileReader } from './csvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let manUtdWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUtdWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
